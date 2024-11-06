a963441562275cf1e9f218db718817b2455c3f91
José Domingo Delgado Alonso <jose.delgado.alonso.al@iespoligonosur.org>
2024-11-06 20:58:30 +0100

    sdad

---
+++ CrearBaseDeDatos.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+usuario= os.getenv("USUARIO_MONGODB")
+password = os.getenv("PASSWORD_MONGODB")
+cluster= os.getenv("CLUSTER_MONGODB")
+
+cliente = MongoClient('mongodb+srv://'+usuario+':'+password+'@'+cluster+'.vhjwo.mongodb.net/?retryWrites=true&w=majority&appName='+cluster)
+
+try:
+   cliente.admin.command('ping')
+   print("NOS CONECTAMOS CORRECTAMENTE")
+except Exception as e:
+   print(e)
+
+baseDatos = cliente["mi_primera_base_datos"]
+
+coleccion = baseDatos["mi_primera_coleccion"]
+
+documento = { "nombre": "Domi", "apellido": "Delgado" }
+
+insercion = coleccion.insert_one(documento)
+
+print(insercion)
+
+
